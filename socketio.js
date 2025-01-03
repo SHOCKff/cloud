34aabede1ff539a58710348d6275fd664ef92431
SHOCKff <156565597+SHOCKff@users.noreply.github.com>
2025-01-04 02:11:55 +0530

    Add files via upload

---
+++ socketio.js
@@ -0,0 +1,37 @@
+const http     = require("http");
+const express  = require("express");
+const path     = require("path");
+const {Server} = require("socket.io");
+
+
+const port    = 9000;
+
+
+const app    = express();
+const server = http.createServer(app);
+const io     = new Server(server);
+
+//socket io handleling
+io.on('connection', (socket) => { 
+  socket.on("User_message",(message)=>{
+    console.log(message);
+    socket.emit("Received_message",message);
+  })
+
+
+});
+
+
+
+
+
+app.use(express.static (path.resolve("./public")));
+
+app.get("/" , (req ,res)=>{
+  return sendFile('../public/index.js');
+});
+
+
+
+
+server.listen(port , () => {console.log(`sever started at port ${port} !`)})
